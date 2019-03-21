910bdb4259fe213ed4767ab1b4b5b6e822559c1e
Chanatip Kriengkraipetch <mikaecat@gmail.com>
2019-03-21 12:00:17 +0700

    TestAddHandler (http)

--- day7/calc/calc.go
+++ day7/calc/calc.go
@@ -1,5 +1,11 @@
 package calc
 
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
 func Add(a, b int) int {
 	return a + b
 }
@@ -14,4 +20,12 @@ func Mul(a, b int) int {
 
 func Div(a, b float64) float64 {
 	return a / b
+}
+
+func AddHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	a, _ := strconv.Atoi(r.FormValue("a"))
+	b, _ := strconv.Atoi(r.FormValue("b"))
+	fmt.Fprintf(w, `{"result":%d}`, a+b)
 }
--- day7/calc/calc_test.go
+++ day7/calc/calc_test.go
@@ -2,6 +2,9 @@ package calc
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -38,4 +41,18 @@ func ExampleAdd() {
 	fmt.Println(n)
 	//Output:
 	//30
+}
+
+func TestAddHandler(t *testing.T) {
+	req := httptest.NewRequest("GET", "http://test.com/add?a=10&b=20", nil)
+	w := httptest.NewRecorder()
+
+	AddHandler(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
+	assert.Equal(t, `{"result":30}`, string(body))
 }
